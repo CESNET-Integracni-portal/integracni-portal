a049f4426a759df2fb47d8d3350d3cda55a807b3
Petr Strnad <strnape1@fel.cvut.cz>
2014-07-07 12:54:47 +0200

    #17 Pripojeni k databazi

--- src/main/java/cz/cvut/fel/integracniportal/controller/HomeController.java
+++ src/main/java/cz/cvut/fel/integracniportal/controller/HomeController.java
@@ -1,5 +1,8 @@
-package cz.cvut.fel.integracniportal;
+package cz.cvut.fel.integracniportal.controller;
 
+import cz.cvut.fel.integracniportal.dao.TestDao;
+import cz.cvut.fel.integracniportal.model.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +15,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/")
 public class HomeController {
 
+    @Autowired
+    TestDao testDao;
+
     @RequestMapping(method = RequestMethod.GET)
     public String home(ModelMap model) {
+        Test test = new Test();
+        test.setText("TEXT");
+        testDao.saveTest(test);
+
         return "home";
     }
 }
