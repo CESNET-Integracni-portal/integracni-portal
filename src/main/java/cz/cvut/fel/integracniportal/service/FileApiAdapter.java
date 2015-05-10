88858393771e939656cafd4935a17ed1da17e84f
Radek Ježdík <redhead@email.cz>
2015-05-10 17:12:55 +0200

    Add grouping folder by organization unit to the CESNET storage

--- src/main/java/cz/cvut/fel/integracniportal/service/FileApiAdapter.java
+++ src/main/java/cz/cvut/fel/integracniportal/service/FileApiAdapter.java
@@ -131,6 +131,7 @@ public class FileApiAdapter {
 
         user.setId(owner.getId());
         user.setUsername(owner.getUsername());
+        user.setOrganizationalUnitId(owner.getOrganizationalUnitId());
 
         return user;
     }
