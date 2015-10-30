3c8b56a34733de309b8c69762ef0bf9d773834ff
Radek Ježdík <redhead@email.cz>
2015-10-30 01:58:44 +0100

    Old tests removed

--- src/test/java/cz/cvut/fel/integracniportal/service/UserDetailsService_Test.groovy
+++ src/test/java/cz/cvut/fel/integracniportal/service/UserDetailsService_Test.groovy
@@ -3,10 +3,7 @@ package cz.cvut.fel.integracniportal.service
 import com.github.springtestdbunit.annotation.DatabaseSetup
 import cz.cvut.fel.integracniportal.AbstractIntegrationTestCase
 import cz.cvut.fel.integracniportal.SpringockitoWebContextLoader
-import cz.cvut.fel.integracniportal.exceptions.IllegalOperationException
-import cz.cvut.fel.integracniportal.exceptions.NotFoundException
 import cz.cvut.fel.integracniportal.model.UserDetails
-import cz.cvut.fel.integracniportal.representation.UserDetailsRepresentation
 import org.junit.Before
 import org.junit.Test
 import org.kubek2k.springockito.annotations.experimental.DirtiesMocks
@@ -69,16 +66,6 @@ public class UserDetailsService_Test extends AbstractIntegrationTestCase {
     }
 
     @Test
-    void "should throw NotFoundException for nonexistent user with id 1000"() {
-        try {
-            userDetailsService.getUserById(1000);
-            fail("User with id 1000 should not exist");
-        } catch (NotFoundException e) {
-            // OK
-        }
-    }
-
-    @Test
     void "should return null for nonexistent user with username 'xyz'"() {
         UserDetails userDetails = userDetailsService.getUserByUsername("xyz");
         assertNull(userDetails);
@@ -112,119 +99,6 @@ public class UserDetailsService_Test extends AbstractIntegrationTestCase {
         assertEquals(usersInUnit.size(), 1);
         assertEquals(usersInUnit.get(0).getId(), "101");
         assertEquals(usersInUnit.get(0).getUsername(), "a");
-    }
-
-    @Test
-    void "should create a new user"() {
-        UserDetailsRepresentation userDetailsRepresentation = new UserDetailsRepresentation();
-        userDetailsRepresentation.setUsername("newUser");
-        userDetailsRepresentation.setPassword("password");
-        userDetailsRepresentation.setUnitId("1");
-        userDetailsService.createUser(userDetailsRepresentation);
-
-        // Confirm that the user has been created
-        UserDetails userDetails = userDetailsService.getUserByUsername("newUser");
-        assertNotNull(userDetails);
-        assertEquals(userDetails.getUsername(), "newUser");
-        assertTrue(passwordEncoder.matches("password", userDetails.getPassword()));
-        assertEquals(userDetails.getOrganizationalUnit().getId(), "1");
-
-        // Confirm that the user has been added to a unit
-        List<UserDetails> usersInUnit = userDetailsService.getAllUsersInOrganizationalUnit("1");
-        boolean userFound = false;
-        for (UserDetails userInUnit: usersInUnit) {
-            if (userInUnit.getUsername().equals("newUser")) {
-                userFound = true;
-                break;
-            }
-        }
-        assertTrue(userFound);
-    }
-
-    @Test
-    void "should update user a's password"() {
-        // Check that the user exists prior to testing
-        UserDetails userDetails = userDetailsService.getUserByUsername("a");
-        assertNotNull(userDetails);
-        userDetails.setPassword(passwordEncoder.encode("myOldPassword"))
-        userDetailsService.saveUser(userDetails)
-
-        userDetailsService.changePassword(token, userDetails.getId(), "myNewPassword", "myOldPassword")
-
-        UserDetails userDetailsUpdated = userDetailsService.getUserByUsername("a");
-        assertNotNull(userDetailsUpdated)
-        assertTrue(passwordEncoder.matches("myNewPassword", userDetailsUpdated.getPassword()));
-    }
-
-    @Test
-    void "should not update user a's password if old password doesn't match"() {
-        // Check that the user exists prior to testing
-        UserDetails userDetails = userDetailsService.getUserByUsername("a");
-        assertNotNull(userDetails);
-        userDetails.setPassword(passwordEncoder.encode("myOldPassword"))
-        userDetailsService.saveUser(userDetails)
-
-        try {
-            userDetailsService.changePassword(token, userDetails.getId(), "myNewPassword", "wrongOldPassword")
-            fail("should have thrown exception")
-        } catch (IllegalOperationException e) {
-            // ok
-        }
-
-        UserDetails userDetails2 = userDetailsService.getUserByUsername("a");
-        assertNotNull(userDetails2)
-        assertTrue(passwordEncoder.matches("myOldPassword", userDetails2.getPassword()));
-    }
-
-    @Test
-    void "should update user a's roles"() {
-        // Check that the user exists prior to testing
-        UserDetails userDetails = userDetailsService.getUserByUsername("a");
-        assertNotNull(userDetails);
-        assertEquals(userDetails.getId(), "101");
-        assertTrue(userDetails.getUserRoles().size() == 1);
-
-        userDetailsService.updateRoles(userDetails.getId(), ["externists", "foo"]);
-
-        UserDetails userDetailsUpdated = userDetailsService.getUserByUsername("a");
-        assertNotNull(userDetailsUpdated)
-        assertTrue(userDetailsUpdated.getUserRoles().size() == 2);
-    }
-
-    @Test
-    void "should update user a's permissions"() {
-        // Check that the user exists prior to testing
-        UserDetails userDetails = userDetailsService.getUserByUsername("a");
-        assertNotNull(userDetails);
-        assertEquals(userDetails.getId(), "101");
-        assertTrue(userDetails.getPermissions().size() == 0);
-
-        userDetailsService.updatePermissions(userDetails.getId(), ["externists", "password"]);
-
-        UserDetails userDetailsUpdated = userDetailsService.getUserByUsername("a");
-        assertNotNull(userDetailsUpdated)
-        assertTrue(userDetailsUpdated.getPermissions().size() == 2);
-    }
-
-    @Test
-    void "should remove user 'a' with id 101"() {
-        // Check that the user exists prior to testing
-        UserDetails userDetails = userDetailsService.getUserByUsername("a");
-        assertNotNull(userDetails);
-        assertEquals(userDetails.getId(), "101");
-        assertEquals(userDetails.getUsername(), "a");
-
-        // Remove the user
-        userDetailsService.removeUser(userDetails);
-
-        // Check that the user no longer exists
-        try {
-            userDetailsService.getUserById("101");
-            fail("User 'a' with id 101 still exists after being removed.");
-        } catch (NotFoundException e) {
-            // OK
-        }
-        assertNull(userDetailsService.getUserByUsername("a"));
     }
 
 }
--- src/test/java/cz/cvut/fel/integracniportal/service/UserRoleService_Test.groovy
+++ src/test/java/cz/cvut/fel/integracniportal/service/UserRoleService_Test.groovy
@@ -4,7 +4,6 @@ import com.github.springtestdbunit.annotation.DatabaseSetup
 import cz.cvut.fel.integracniportal.AbstractIntegrationTestCase
 import cz.cvut.fel.integracniportal.SpringockitoWebContextLoader
 import cz.cvut.fel.integracniportal.domain.Permission
-import cz.cvut.fel.integracniportal.exceptions.NotFoundException
 import cz.cvut.fel.integracniportal.model.UserRole
 import org.junit.Test
 import org.kubek2k.springockito.annotations.experimental.DirtiesMocks
@@ -55,16 +54,6 @@ public class UserRoleService_Test extends AbstractIntegrationTestCase {
     }
 
     @Test
-    void "should throw NotFoundException for nonexistent user role with id 1000"() {
-        try {
-            userRoleService.getRoleById(1000);
-            fail("User role with id 1000 should not exist");
-        } catch (NotFoundException e) {
-            // OK
-        }
-    }
-
-    @Test
     void "should return null for nonexistent user role with name 'xyz'"() {
         UserRole userRole = userRoleService.getRoleByName("xyz");
         assertNull(userRole);
@@ -79,77 +68,6 @@ public class UserRoleService_Test extends AbstractIntegrationTestCase {
         assertEquals(1, userRoleList.get(0).getPermissions().size());
         Permission permission = (Permission) userRoleList.get(0).getPermissions().toArray()[0];
         assertEquals("externists", permission.toString());
-    }
-
-    @Test
-    void "should create a new role with permissions 'password' and 'externists'"() {
-        UserRole userRole = new UserRole();
-        userRole.setName("test");
-        userRole.setDescription("Test role.");
-        Set<Permission> permissions = new HashSet<>();
-        permissions.add(Permission.CHANGE_PASSWORD);
-        permissions.add(Permission.EDIT_EXTERNISTS);
-        userRole.setPermissions(permissions);
-        userRoleService.createRole(userRole);
-
-        // Check that the role was created
-        UserRole retrievedRole = userRoleService.getRoleByName("test");
-        assertNotNull(retrievedRole);
-        assertEquals("test", retrievedRole.getName());
-        assertEquals("Test role.", retrievedRole.getDescription());
-        assertEquals(2, retrievedRole.getPermissions().size());
-        assertTrue(retrievedRole.getPermissions().contains(Permission.EDIT_EXTERNISTS));
-        assertTrue(retrievedRole.getPermissions().contains(Permission.CHANGE_PASSWORD));
-    }
-
-    @Test
-    void "should update user role 'externists'"() {
-        // Check that the user role exists prior to testing
-        UserRole userRole = userRoleService.getRoleByName("externists");
-        assertNotNull(userRole);
-        assertEquals("101", userRole.getId());
-        assertEquals("externists", userRole.getName());
-        assertEquals("Can edit externists.", userRole.getDescription());
-
-        // Update the role
-        userRole.setName("changedRole");
-        userRole.setDescription("New description.");
-        userRole.getPermissions().remove(Permission.EDIT_EXTERNISTS);
-        userRole.getPermissions().add(Permission.CHANGE_PASSWORD);
-        userRoleService.saveRole(userRole);
-
-        // Check that the role is no longer available under the old name
-        UserRole userRoleOld = userRoleService.getRoleByName("externists");
-        assertNull(userRoleOld);
-
-        // Check that the role has been changed
-        UserRole userRoleUpdated = userRoleService.getRoleByName("changedRole");
-        assertEquals("101", userRoleUpdated.getId());
-        assertEquals("changedRole", userRoleUpdated.getName());
-        assertEquals("New description.", userRoleUpdated.getDescription());
-        assertEquals(1, userRoleUpdated.getPermissions().size());
-        assertTrue(userRoleUpdated.getPermissions().contains(Permission.CHANGE_PASSWORD));
-    }
-
-    @Test
-    void "should remove user role 'foo'"() {
-        // Check that the user role exists prior to testing
-        UserRole userRole = userRoleService.getRoleByName("foo");
-        assertNotNull(userRole);
-        assertEquals(102, userRole.getId());
-        assertEquals("foo", userRole.getName());
-
-        // Remove the user role
-        userRoleService.deleteRole(userRole);
-
-        // Check that the user role no longer exists
-        try {
-            userRoleService.getRoleById(102);
-            fail("User role 'foo' with id 102 still exists after being removed.");
-        } catch (NotFoundException e) {
-            // OK
-        }
-        assertNull(userRoleService.getRoleByName("foo"));
     }
 
 }
